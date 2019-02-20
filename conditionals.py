7320b7bb13be12b0027d1e2200bda9a303c41584
Simranpal Singh <simranpal.singh@suse.com>
2019-02-20 14:48:35 -0800

    if else and defining code block

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+age = 31
+
+if age>= 35:
+    print('You are old enough to be president!')
+elif age >=30:
+    print('You are old enough to become Senator!')
+else:
+    print('You are not old enough to be elected')
+
+print('Have a nice day!!')
