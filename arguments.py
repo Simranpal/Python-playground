5e0be74c2a8775c3f656ff2a2ec371a1ac8b3b41
Simranpal Singh <simranpal.singh@suse.com>
2019-03-29 09:11:57 -0700

    more tests with *args

--- arguments.py
+++ arguments.py
@@ -4,3 +4,22 @@ def test_var_args(farg, *args):
         print "another arg:", arg
 
 test_var_args(1, "two", 3)
+print("__________")
+
+
+test_var_args(1, "another one")
+print("__________")
+
+test_var_args(False, 4.55)
+print("__________")
+
+def multiply(*args):
+    ans = 1
+    for num in args:
+        ans *= num
+    print(ans)
+
+multiply(4, 5)
+multiply(10, 9)
+multiply(2, 3, 4)
+multiply(3, 5, 10, 6)
