01b611b4255425c81fca7c229ad28ebd0dc4204a
Simranpal Singh <simranpal.singh@suse.com>
2019-03-29 09:48:13 -0700

    correct way to call fn with *8kwargs

--- kwargs.py
+++ kwargs.py
@@ -1,4 +1,15 @@
 def print_kwargs(**kwargs):
         print(kwargs)
 
-print_kwargs(kwargs_1="Shark", kwargs_2=4.5, kwargs_3=True)
+print_kwargs(kwargs_1="Sim", kwargs_2=4.5, kwargs_3=True)
+print_kwargs()
+
+"""
+Below will generate error 
+# TypeError: print_kwargs() takes exactly 0 arguments (1 given)
+"""
+#print_kwargs("Sim")
+
+myargs = {"arg3": 3, "arg2": "two"}
+
+print_kwargs(**myargs)
